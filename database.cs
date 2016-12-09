4298769688c3483adf2abafdf69f6986c74507ec
Muhammad Saad Bin Ghayas <msbg93@users.noreply.github.com>
2016-12-09 14:36:25 +0500

    Add files via upload

---
+++ database.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for Cls_database
+/// </summary>
+public class database
+{
+    
+	public database()
+	{
+	}
+
+    public static DataTable GetTable(string query)
+    {
+        //string dbcon = ConfigurationManager.ConnectionStrings["Sebis"].ConnectionString;
+        //SqlConnection con = new SqlConnection(dbcon);
+        try
+        {
+        DataTable dt = new DataTable();
+        
+        //con.Open();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        adapter.SelectCommand = new SqlCommand(query, Connection.GetConnection());
+        adapter.Fill(dt);
+       // con.Close();
+        return dt;
+           }
+        catch (Exception)
+        {
+             throw;
+        }
+    }
+
+    public static void ExecuteQuery(string query)
+    {
+        //string dbcon = ConfigurationManager.ConnectionStrings["Sebis"].ConnectionString;
+        //SqlConnection con = new SqlConnection(dbcon);
+        //con.Open();
+        try
+        {
+        SqlCommand cmd = new SqlCommand(query, Connection.GetConnection());
+        cmd.CommandTimeout = 50000000;
+        cmd.ExecuteNonQuery();
+         }
+        catch (Exception)
+        {
+             throw;
+        }
+ 
+
+    }
+   
+}
